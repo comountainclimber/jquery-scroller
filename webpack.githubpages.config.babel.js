74c1f548d9de88a000118e0e4fb91856d77c5bfe
comountainclimber <mlasky46@gmail.com>
2017-10-30 13:50:03 -0600

    added support for github pages

---
+++ webpack.githubpages.config.babel.js
@@ -0,0 +1,75 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CleanPlugin from 'clean-webpack-plugin';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import WebpackOnBuildPlugin from 'on-build-webpack';
+import ghpages from 'gh-pages';
+
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.[hash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            { loader: 'css-loader', options: { importLoaders: 1 } },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: () => [require('autoprefixer')({ browsers: ['> 5%'] })]
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(png|jpg|gif|ico)$/,
+        loader: 'file-loader?limit=10000&name=[name].[ext]'
+      },
+      {
+        test: /\.woff|\.woff2|\.svg|\.eot|\.ttf/,
+        loader: 'file-loader?prefix=font/&limit=10000&name=[name].[ext]'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.[hash].css'),
+    new CleanPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: 'head',
+      filename: 'index.html',
+      favicon: 'src/favicon.ico'
+    }),
+    new WebpackOnBuildPlugin(function(stats) {
+      console.log(stats);
+      ghpages.publish('dist', err => (err ? console.log('An error occurred deploying to github pages:', err) :
+        console.log('Succesfully deployed to github pages.')
+      ));
+    }),
+  ]
+};
